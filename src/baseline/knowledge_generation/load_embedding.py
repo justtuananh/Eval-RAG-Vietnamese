ac1e4ae0f238714aa2dca7251c5ca896c8a39470
Duy Ta <duyfaker01@gmail.com>
2024-08-28 23:14:20 +0700

    index

--- src/baseline/knowledge_generation/load_embedding.py
+++ src/baseline/knowledge_generation/load_embedding.py
@@ -1,13 +1,12 @@
 from knowledge_generation.ingest import load_embeddings
 from docs_process import docs_split
-from docs_process.dataset_load import load_eval_data
+from docs_process.dataset_load import document_transform, load_eval_data
 
 
 
 
-if __name__ == "__main__" :
-    dataset  = load_eval_data("legal")
-    doc_splited = docs_split.split_documents(768, load_eval_data("legal"), tokenizer_name = "", is_tiktoken = True)
+if __name__ == "__main__" : 
+    doc_splited = docs_split.split_documents(768, document_transform(load_eval_data("legal")), tokenizer_name = "", is_tiktoken = True)
     knowledge_index = load_embeddings(
         doc_splited,
         512,
