72c6cd69926f34f7901f5a2368d0f88ee45a2c76
Duy Ta <duyfaker01@gmail.com>
2024-08-29 02:09:39 +0700

    baseline

--- src/baseline/llms/groq_chain.py
+++ src/baseline/llms/groq_chain.py
@@ -2,6 +2,7 @@ import os
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 
-llm_api = load_dotenv("../.env")
+llm_api = load_dotenv(".env")
 llm_api_key = os.getenv("llm_api")
-llm = ChatGroq(model_name="llama3-70b-8192", temperature=0,api_key= llm_api_key, request_timeout = 120 , max_retries = 20) ## Replace to real LLMs (Cohere / Groq / OpenAI)
+print(llm_api_key)
+llm = ChatGroq(model_name="llama3-70b-8192", temperature=0,api_key= llm_api, request_timeout = 120 , max_retries = 20) ## Replace to real LLMs (Cohere / Groq / OpenAI)
